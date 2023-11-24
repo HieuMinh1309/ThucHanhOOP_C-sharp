a03b5336dbb639b9d4031f8dbc603af79870d1c1
HieuMinh1309 <Hieuminh091304@gmail.com>
2023-11-24 13:53:52 +0700

    Create Class Student, Lib

---
+++ OOP/Student.cs
@@ -0,0 +1,23 @@
+﻿
+namespace OOP;
+internal class Student
+{
+    //các field
+    int? id;
+    string? fullname;
+    bool gender;
+    DateOnly dob;
+
+    //constructor
+    public Student(int? id, string? fullname, bool gender, DateOnly dob)
+    {
+        this.id = id;
+        this.fullname = fullname;
+        this.gender = gender;
+        this.dob = dob;
+    }
+
+    public Student() { }
+
+
+}
